38401744017affed12b23ae613c2dc513c7ed771
Čedomir Stevanović <cedomirstevanovic8@gmail.com>
2024-04-09 13:47:36 +0200

    test: Implemented create key point test

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/ImageDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class ImageDto
+    {
+        public string Filename { get; set; }
+        public string Data { get; set; }
+    }
+}
