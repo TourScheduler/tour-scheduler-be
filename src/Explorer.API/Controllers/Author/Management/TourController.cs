b375e127bc9944cfad9af3249d2c39358cbf82f2
Čedomir Stevanović <cedomirstevanovic8@gmail.com>
2024-04-15 20:58:39 +0200

    test: Implemented filter by status tests

--- src/Explorer.API/Controllers/Author/Management/TourController.cs
+++ src/Explorer.API/Controllers/Author/Management/TourController.cs
@@ -52,5 +52,11 @@ namespace Explorer.API.Controllers.Author.Tours
             var result = _tourService.Archive(id);
             return CreateResponse(result);
         }
+
+        [HttpGet("author/{authorId:int}/filter")]
+        public ActionResult<TourDto> FilterByStatus(int authorId, [FromQuery] int status)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
