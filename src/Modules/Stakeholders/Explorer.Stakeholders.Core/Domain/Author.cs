2f7fc4b56c4a6f30365fee0a46017561cd3a6667
Čedomir Stevanović <cedomirstevanovic8@gmail.com>
2024-06-03 12:37:07 +0200

    feat: Implemented best author add points and awarded status

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Author.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Author.cs
@@ -1,4 +1,5 @@
 ﻿using Explorer.BuildingBlocks.Core.Domain;
+using Explorer.Tours.API.Dtos;
 using Explorer.Tours.Core.Domain;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,8 @@ namespace Explorer.Stakeholders.Core.Domain
     public class Author : Entity
     {
         public long UserId { get; init; }
+        public int Points { get; private set; }
+        public bool IsAwarded { get; private set; }
         public List<Tour> Tours { get; init; }
 
         public Author()
@@ -18,15 +21,30 @@ namespace Explorer.Stakeholders.Core.Domain
             Tours = new List<Tour>();
         }
 
-        public Author(long userId, List<Tour> tours)
+        public Author(long userId, int points, bool isAwarded, List<Tour> tours)
         {
             UserId = userId;
             Tours = tours;
+            Points = points;
+            IsAwarded = isAwarded;
             Validate();
         }
         private void Validate()
         {
             if (UserId == 0) throw new ArgumentException("Invalid UserId");
+        }
+
+        public void AddPoints()
+        {
+            Points++;
+        }
+
+        public void Award()
+        {
+            if (IsAwarded == false && Points >= 5)
+            {
+                IsAwarded = true;
+            }
         }
     }
 }
